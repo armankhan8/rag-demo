e48d967082f92ea94a8d182889dd0b92ef4299c6
armankhan <armaankhan813085@gmail.com>
2026-06-27 21:43:36 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Git & GitHub - Push code to GitHub
+
+print("Git tracks your code changes locally.")
+print("GitHub stores your code online.")
+print("\nSteps to push code:")
+print("1. git init")
+print("2. git add .")
+print("3. git commit -m 'your message'")
+print("4. git remote add origin https://github.com/USER/REPO.git")
+print("5. git push -u origin main")
